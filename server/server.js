80e4a34ed0fec76a8edc78d886a0575c627cc8d6
anurag-2420 <anuragpatel20june@gmail.com>
2023-11-26 01:02:49 +0530

    Changes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ import 'dotenv/config';
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://login-signup-42cm.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 app.use(cors(
     {
         origin:["https://login-signup-42cm.vercel.app/"],
